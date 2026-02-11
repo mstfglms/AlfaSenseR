7f37050d1096bff3aed40b4612646aea2d2a2cf8
Mustafa Gulmus <mstfglms@gmail.com>
2026-02-11 23:37:50 +0300

    rev3

--- include/Thermistor.h
+++ include/Thermistor.h
@@ -7,3 +7,4 @@
 float readVoltage(uint8_t pin);
 float calculateTemperature(float resistance, const SteinhartHart& coefficients);
 float readTemperature(uint8_t pin, SensorType sensorType);
+float voltageToResistance(float voltage);
